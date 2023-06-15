7cf38836b6428f237bb1073123cb29c62ef9a176
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-14 21:25:34 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jorge Henrique on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
